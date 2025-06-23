f20aae2117cd498858eb14682bee4740093bda48
AnoshArdeni <aardeni@outlook.com>
2025-06-23 15:40:05 -0700

    Added port

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from database import get_db_connection, initialize_db
 import requests
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -108,6 +109,6 @@ def delete_entry(id):
     return jsonify({"message": "Entry deleted successfully"}), 200
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
